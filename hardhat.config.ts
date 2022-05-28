7db8be01ec0150d24b12033bf0789414cd25f7b0
Endalkachew <eb808826@gmail.com>
2022-05-28 16:09:49 +0300

    Removed files

--- hardhat.config.ts
+++
@@ -1,9 +0,0 @@
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-ethers";
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-    solidity: "0.8.0",
-};
